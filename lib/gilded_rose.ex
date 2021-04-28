9d3e3921c086be88a9f99c6857e197107d28d320
Zac Barnes <zbarnes757@gmail.com>
2021-04-28 18:20:23 -0500

    add tests to preserve current functionality

--- lib/gilded_rose.ex
+++ lib/gilded_rose.ex
@@ -2,18 +2,8 @@ defmodule GildedRose do
   use Agent
   alias GildedRose.Item
 
-  def new() do
-    {:ok, agent} =
-      Agent.start_link(fn ->
-        [
-          Item.new("+5 Dexterity Vest", 10, 20),
-          Item.new("Aged Brie", 2, 0),
-          Item.new("Elixir of the Mongoose", 5, 7),
-          Item.new("Sulfuras, Hand of Ragnaros", 0, 80),
-          Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-          Item.new("Conjured Mana Cake", 3, 6)
-        ]
-      end)
+  def new(items \\ default_items()) do
+    {:ok, agent} = Agent.start_link(fn -> items end)
 
     agent
   end
@@ -134,5 +124,18 @@ defmodule GildedRose do
     end
 
     :ok
+  end
+
+  # Helpers
+
+  defp default_items do
+    [
+      Item.new("+5 Dexterity Vest", 10, 20),
+      Item.new("Aged Brie", 2, 0),
+      Item.new("Elixir of the Mongoose", 5, 7),
+      Item.new("Sulfuras, Hand of Ragnaros", 0, 80),
+      Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+      Item.new("Conjured Mana Cake", 3, 6)
+    ]
   end
 end
--- test/gilded_rose_test.exs
+++ test/gilded_rose_test.exs
@@ -1,10 +1,84 @@
 defmodule GildedRoseTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest GildedRose
+
+  alias GildedRose.Item
 
   test "interface specification" do
     gilded_rose = GildedRose.new()
     [%GildedRose.Item{} | _] = GildedRose.items(gilded_rose)
     assert :ok == GildedRose.update_quality(gilded_rose)
+  end
+
+  test "sell_in and quality should decrease by 1 under normal circumstances" do
+    items = [Item.new("+5 Dexterity Vest", 10, 20)]
+    gilded_rose = GildedRose.new(items)
+    assert :ok == GildedRose.update_quality(gilded_rose)
+    assert [%GildedRose.Item{sell_in: 9, quality: 19}] = GildedRose.items(gilded_rose)
+  end
+
+  test "quality should reduce twice as fast when sell_in is less than 0" do
+    items = [Item.new("+5 Dexterity Vest", 0, 20)]
+    gilded_rose = GildedRose.new(items)
+    assert :ok == GildedRose.update_quality(gilded_rose)
+    assert [%GildedRose.Item{sell_in: -1, quality: 18}] = GildedRose.items(gilded_rose)
+  end
+
+  test "\"Aged Brie\" should increase in quality each day" do
+    items = [Item.new("Aged Brie", 2, 0)]
+    gilded_rose = GildedRose.new(items)
+    assert :ok == GildedRose.update_quality(gilded_rose)
+    assert [%GildedRose.Item{sell_in: 1, quality: 1}] = GildedRose.items(gilded_rose)
+  end
+
+  test "quality should never be below 0" do
+    items = [Item.new("+5 Dexterity Vest", 0, 0)]
+    gilded_rose = GildedRose.new(items)
+    assert :ok == GildedRose.update_quality(gilded_rose)
+    assert [%GildedRose.Item{sell_in: -1, quality: 0}] = GildedRose.items(gilded_rose)
+  end
+
+  test "quality should never be above 50" do
+    items = [Item.new("Aged Brie", 2, 50)]
+    gilded_rose = GildedRose.new(items)
+    assert :ok == GildedRose.update_quality(gilded_rose)
+    assert [%GildedRose.Item{sell_in: 1, quality: 50}] = GildedRose.items(gilded_rose)
+  end
+
+  test "\"Sulfuras\" should not change sell_in or quality" do
+    items = [Item.new("Sulfuras, Hand of Ragnaros", 0, 80)]
+    gilded_rose = GildedRose.new(items)
+    assert :ok == GildedRose.update_quality(gilded_rose)
+    assert [%GildedRose.Item{sell_in: 0, quality: 80}] = GildedRose.items(gilded_rose)
+  end
+
+  describe "\"Backstage passes\"" do
+    test "should increase in quality by 1 when sell_in is greater than 10" do
+      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)]
+      gilded_rose = GildedRose.new(items)
+      assert :ok == GildedRose.update_quality(gilded_rose)
+      assert [%GildedRose.Item{sell_in: 14, quality: 21}] = GildedRose.items(gilded_rose)
+    end
+
+    test "should increase in quality by 2 when sell_in is greater than 5 but less or equal to 10" do
+      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 20)]
+      gilded_rose = GildedRose.new(items)
+      assert :ok == GildedRose.update_quality(gilded_rose)
+      assert [%GildedRose.Item{sell_in: 9, quality: 22}] = GildedRose.items(gilded_rose)
+    end
+
+    test "should increase in quality by 3 when sell_in is greater than or equal to 0 but less or equal to 5" do
+      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 20)]
+      gilded_rose = GildedRose.new(items)
+      assert :ok == GildedRose.update_quality(gilded_rose)
+      assert [%GildedRose.Item{sell_in: 4, quality: 23}] = GildedRose.items(gilded_rose)
+    end
+
+    test "should have no quality when sell_in is less than 0" do
+      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 20)]
+      gilded_rose = GildedRose.new(items)
+      assert :ok == GildedRose.update_quality(gilded_rose)
+      assert [%GildedRose.Item{sell_in: -1, quality: 0}] = GildedRose.items(gilded_rose)
+    end
   end
 end
